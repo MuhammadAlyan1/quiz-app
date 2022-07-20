34c751363b638d5ea2e20a6f9d00fa426e28fc60
Muhammad Alyan <alyan0332@gmail.com>
2022-07-20 13:46:37 +0500

    convert to pure function

--- src/utils/shuffleArray.js
+++ src/utils/shuffleArray.js
@@ -1,8 +1,11 @@
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i -= 1) {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
   }
+
+  return [...newArray];
 }
 
 export default shuffle;
