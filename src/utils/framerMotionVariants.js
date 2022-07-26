9c60d6754dda1b220675e27e39e39426c904c3aa
Muhammad Alyan <alyan0332@gmail.com>
2022-07-26 15:24:06 +0500

    add framer motion variants

---
+++ src/utils/framerMotionVariants.js
@@ -0,0 +1,100 @@
+export const openNewSection = {
+  hidden: {
+    opacity: 0,
+    scale: 0.4,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.1,
+      duration: 0.2,
+    },
+  },
+};
+
+export const QuizSelectionSubCategoryHeading = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+    },
+  },
+};
+
+export const amountInput = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+    },
+  },
+};
+
+export const QuizSelectionSubCategoryNextSectionButton = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+  },
+  whileHover: {
+    scale: 1.2,
+  },
+};
+
+export const quizSelectionOptions = {
+  hidden: {
+    scale: 0,
+  },
+  visible: {
+    scale: 1,
+  },
+  whileHover: {
+    scale: 1.05,
+  },
+  whileTap: {
+    scale: 0.95,
+  },
+};
+
+// ==============
+// Quiz
+// ==============
+
+export const quizSection = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+export const quizContents = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+    },
+  },
+};
